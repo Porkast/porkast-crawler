4b56b1ffbc6517cef24b94351c8dfca02060ee2d
chenjunqian <chenjunqian0810@foxmail.com>
2023-03-16 00:10:14 +0800

    feat: add ximalaya target url

---
+++ internal/consts/ximalaya.go
@@ -0,0 +1,25 @@
+package consts
+
+const XIMALAYA_PODCAST_WORKER = "XIMALAYA_PODCAST_WORKER"
+const XIMALAYA_ENTRY_WORKER = "XIMALAYA_ENTRY_WORKER"
+
+// 全部
+const XIMALAYA_CATAGORY_URL = "https://www.ximalaya.com/revision/category/v2/albums?pageNum=%d&pageSize=56&sort=1"
+
+// 热点
+const XIMALAYA_CATAGORY_URL_HOT = "https://www.ximalaya.com/revision/category/v2/albums?pageNum=%d&pageSize=56&sort=1&categoryId=1005"
+
+// 音乐
+const XIMALAYA_CATAGORY_URL_MUSIC = "https://www.ximalaya.com/revision/category/v2/albums?pageNum=%d&pageSize=56&sort=1&categoryId=2"
+
+// 娱乐
+const XIMALAYA_CATAGORY_URL_ENTERTAINENT = "https://www.ximalaya.com/revision/category/v2/albums?pageNum=%d&pageSize=56&sort=1&categoryId=4"
+
+// 外语
+const XIMALAYA_CATAGORY_URL_FOREIGN_LANGUAGE = "https://www.ximalaya.com/revision/category/v2/albums?pageNum=%d&pageSize=56&sort=1&categoryId=5"
+
+// 儿童
+const XIMALAYA_CATAGORY_URL_CHILDREN = "https://www.ximalaya.com/revision/category/v2/albums?pageNum=%d&pageSize=56&sort=1&categoryId=6"
+
+// 商业财经
+const XIMALAYA_CATAGORY_URL_BUSINESS = "https://www.ximalaya.com/revision/category/v2/albums?pageNum=%d&pageSize=56&sort=1&categoryId=8"
