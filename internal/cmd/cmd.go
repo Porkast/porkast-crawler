2a4bbf6bd8d968d46513c9b0026e0c55bbd0ab18
chenjunqian <chenjunqian0810@foxmail.com>
2023-03-22 23:50:48 +0800

    feat: category worker

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -31,8 +31,10 @@ func initCelery(ctx context.Context) {
 }
 
 func RegisterCeleryWorker() {
+	// XIMALAYA
 	celery.GetClient().Register(consts.XIMALAYA_PODCAST_WORKER, worker.ParseXiMaLaYaPodcast)
 	celery.GetClient().Register(consts.XIMALAYA_ENTRY_WORKER, worker.ParseXiMaLaYaEntry)
+	// LIZHI FM
 	celery.GetClient().Register(consts.LIZHI_ENTRY_WORKER, worker.ParseLizhiAllCategories)
 	celery.GetClient().Register(consts.LIZHI_CATEGORY_PARSE_WORKER, worker.ParseLizhiPodcastByCategoryPage)
 	celery.GetClient().Register(consts.LIZHI_FM_RSS_URL, worker.ParseLizhiPodcastRSS)
