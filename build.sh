7262fcf7e92116b4c94b762e73ce4f9fe58b128e
chenjunqian <chenjunqian0810@foxmail.com>
2023-04-03 00:26:58 +0800

    feat: add temp pack file

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
-gf pack resource,manifest internal/packed/packed.go -n packed -y
+gf pack resource,manifest internal/packed/data.go -n packed -y
 gf build main.go -n guoshaofm-crawler -trimpath -a amd64 -s linux,darwin -p ./bin
+rm -f internal/packed/data.go 
