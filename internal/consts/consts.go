c0957050475a14c769efdc93505e47055181f490
chenjunqian <chenjunqian0810@foxmail.com>
2023-07-29 23:07:57 +0800

    refactor: every 12 hour crawl podcast website

--- internal/consts/consts.go
+++ internal/consts/consts.go
@@ -1,4 +1,4 @@
 package consts
 
-const PODCAST_WEB_CRAWLER_CRON_PATTERN = "0 0 */6 * * *"
+const PODCAST_WEB_CRAWLER_CRON_PATTERN = "0 0 */12 * * *"
 const FEED_UPDATE_CRON_PATTERN = "0 0 */3 * * *"
