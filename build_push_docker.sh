ea333d91ed18278f8e80e5feb9bc4b72cae92424
chenjunqian <chenjunqian0810@foxmail.com>
2023-08-03 23:18:21 +0800

    feat: add build script

--- build_push_docker.sh
+++ build_push_docker.sh
@@ -1,4 +1,6 @@
-./build.sh
+gf pack resource,manifest internal/packed/data.go -n packed -y
+gf build main.go -n guoshaofm-crawler -trimpath -a amd64 -s linux,darwin -p ./bin
+rm -f internal/packed/data.go 
 
 echo "remove guoshaofm-cralwer image"
 docker rmi beegedelow/guoshaofm-crawler
