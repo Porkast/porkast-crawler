27deafc314d0f4eb377ea6399e0fa495b80f0dc6
chenjunqian <chenjunqian0810@foxmail.com>
2023-08-06 18:20:00 +0800

    refactor: update podcast web crawler cron

--- internal/consts/consts.go
+++ internal/consts/consts.go
@@ -1,4 +1,4 @@
 package consts
 
-const PODCAST_WEB_CRAWLER_CRON_PATTERN = "0 0 */12 * * *"
+const PODCAST_WEB_CRAWLER_CRON_PATTERN = "0 0 */4 * * *"
 const FEED_UPDATE_CRON_PATTERN = "0 0 */2 * * *"
