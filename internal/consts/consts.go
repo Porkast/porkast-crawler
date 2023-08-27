c8de6dc93022a2c2784d027f1498902b5f381dd0
chenjunqian <chenjunqian0810@foxmail.com>
2023-08-27 23:47:14 +0800

    refactor: update schedule

--- internal/consts/consts.go
+++ internal/consts/consts.go
@@ -1,4 +1,4 @@
 package consts
 
-const PODCAST_WEB_CRAWLER_CRON_PATTERN = "0 0 */4 * * *"
-const FEED_UPDATE_CRON_PATTERN = "0 0 */2 * * *"
+const PODCAST_WEB_CRAWLER_CRON_PATTERN = "0 0 */8 * * *"
+const FEED_UPDATE_CRON_PATTERN = "0 0 */4 * * *"
