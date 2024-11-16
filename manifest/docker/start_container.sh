17d33e79f3d6a4fc5c3550a5d0eca453a67ceee1
chenjunqian <chenjunqian0810@foxmail.com>
2024-11-17 04:01:10 +0800

    update config

--- manifest/docker/start_container.sh
+++
@@ -1,7 +0,0 @@
-LOGS_DIR=/home/porkast-crawler/logs
-if [[ ! -e $LOGS_DIR ]]; then
-    mkdir -p $LOGS_DIR
-elif [[ ! -d $LOGS_DIR ]]; then
-    echo "$LOGS_DIR already exists but is not a directory" 1>&2
-fi
-docker run --name porkast-crawler --network host --log-opt max-size=500m --log-opt max-file=3 -v $LOGS_DIR:/app/logs -d beegedelow/porkast-crawler
